e1c435668481511bdf4c4f7a512f997c2c04025a
Carlos Eduardo <2005carloseduardosantos@gmail.com>
2023-07-10 17:48:20 -0300

    started

---
+++ src/js/App.js
@@ -0,0 +1,22 @@
+
+// btn mobile 
+const btnMobile = document.getElementById('btn-mobile');
+
+        function toggleMenu(event){
+            if (event.type == 'touchstart') event.preventDefault();
+            const nav = document.getElementById('nav');
+            nav.classList.toggle('active');
+            const active = nav.classList.contains('active')
+            event.currentTarget.setAttribute('aria-expanded', active);
+            if (active) {
+            event.currentTarget.setAttribute('aria-label', 'fechar menu');
+            } else {
+                event.currentTarget.setAttribute('aria-label', 'abrir menu menu');
+            }
+        }
+
+        btnMobile.addEventListener('click', toggleMenu);
+        btnMobile.addEventListener('touchstart', toggleMenu);
+
+
+//
